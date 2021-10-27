840501fe1770145a5ceba15ea29a80e91cb2db11
Nicolai Truong <nicolai-1@live.dk>
2021-10-27 16:45:44 +0200

    updated solution

--- Exercise 4/Exercise 4.py
+++ Exercise 4/Exercise 4.py
@@ -10,7 +10,7 @@ from tensorflow.python.keras.models import Sequential
 
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
 num_classes = 10
-EPOCHS = 10
+EPOCHS = 1
 train_labels = to_categorical(train_labels, num_classes)
 test_labels = to_categorical(test_labels, num_classes)
 
@@ -21,6 +21,7 @@ train_data = train_data.astype('float')
 
 # TODO: visualize the filters
 
+# The following model will help avoiding overfitting to the data.
 model = Sequential()
 model.add(Conv2D(32, (3,3), activation='relu',input_shape=(28,28,1) , kernel_regularizer=regularizers.l2(0.001)))
 model.add(Dropout(0.25))
@@ -28,7 +29,7 @@ model.add(MaxPooling2D(2,2))
 model.add(Conv2D(64, (3,3), activation='relu', kernel_regularizer=regularizers.l2(0.001)))
 model.add(Dropout(0.25))
 model.add(MaxPooling2D(2,2))
-model.add(Conv2D(64, (3,3), activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+model.add(Conv2D(128, (3,3), activation='relu', kernel_regularizer=regularizers.l2(0.001)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 
@@ -36,7 +37,20 @@ model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.001)
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+filters = model.layers[0].get_weights()[0]
+n_filters = 32
+fig, axs = plt.subplots(8,4)
+for i in range(n_filters):
+	f = filters[:,:,:,1].reshape([3,3])
+	axs[i//4,i%4].imshow(f, cmap="gray")
+plt.show()
+
+plt.savefig('filter.png')
+plt.close()
 history = model.fit(train_data, train_labels, epochs=EPOCHS, batch_size=64, validation_data=(test_data, test_labels))
+
+
 
 print('Model accuracy: ' + str(model.evaluate(test_data, test_labels)[1]))
 
@@ -50,4 +64,4 @@ plt.ylabel('Loss / Accuracy')
 plt.legend()
 plt.show()
 
-plt.savefig('graph.png')
+#plt.savefig('graph.png')
